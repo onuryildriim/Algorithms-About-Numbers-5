1d07a3898be7ab65aae46066ffe84d5f788d3160
Onur Yıldırım <onur.yildirim.2017@ogr.iu.edu.tr>
2022-02-19 00:34:49 +0300

    Create MainPage.java

---
+++ MainPage.java
@@ -0,0 +1,10 @@
+public class MainPage {
+
+	public static void main(String[] args) {
+
+		//Algorithm to find whether the entered number is square or not
+		Algorithm algorithm  = new Algorithm();
+		algorithm.squareNum();
+		
+	}
+}
